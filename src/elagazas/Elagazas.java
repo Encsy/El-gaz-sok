31fd3f9ec82221cbdc93c7bf8cc154680134d3de
Encsi :) <madlics.eniko1@gmail.com>
2025-02-17 13:10:05 +0100

    Összehasonlító műveletek

--- src/elagazas/Elagazas.java
+++ src/elagazas/Elagazas.java
@@ -4,6 +4,8 @@
  */
 package elagazas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
@@ -14,7 +16,53 @@ public class Elagazas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int szam = 5;
+       
+       // Elágazások if-else
+       if(szam >=0){
+           System.out.println("A szám pozitív.");
+       }
+       else{
+           System.out.println("A szám negatív.");
+       }
+       boolean feltetel = (szam >=0);
+       // Ha igaz
+       // feltetel = true
+       // !feltetel = false
+       if(feltetel){
+           System.out.println("A szám pozitív");
+       }
+       //Különben
+       else{
+           System.out.println("A szám pozitív");
+       }
+       
+       //Összehasonlító műveletek
+       /*
+       Egyenlőség: ==
+       Tagadás: !
+       Nem egyenlő: !=
+       Kisebb: a < b
+       Nagyobb vagy egyenlő: a <= b
+       Nagyobb: a > b
+       Nagyobb vagy egyenlő: a >= b
+       */
+       
+       //Kérjünk be két számot, írjuk ki a nagyobbat
+       System.out.println("Kérek egy számot:");
+       Scanner be = new Scanner(System.in); //Adatbekérés
+       int a = be.nextInt(); // be.nextInt() számot vár a felhasználótól
+       System.out.println ("Kérek egy másik számot:");
+       int b = be.nextInt();
+       // Döntsük el, hogy melyik nagyobb
+       if (a < b){
+           System.out.printf("A %d nagyobb, mint %d", b, a);
+       }
+       else{
+           System.out.printf("A %d nagyobb, mint %d", a, b);
+       }
+       
+       
     }
     
 }
